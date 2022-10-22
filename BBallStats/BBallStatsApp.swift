27d965f51ee82d5061d909315bde1665d88fb713
Chad Wallace <cewall0@gmail.com>
2022-10-21 20:21:32 -0400

    First commit

---
+++ BBallStats/BBallStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBallStatsApp.swift
+//  BBallStats
+//
+//  Created by Chad Wallace on 10/21/22.
+//
+
+import SwiftUI
+
+@main
+struct BBallStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
