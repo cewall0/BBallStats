2490bd4e1059cf0e050de27a8eac4fedfa2d81ff
Chad Wallace <cewall0@gmail.com>
2022-10-27 12:37:31 -0400

    Buttons on main screen

---
+++ BBallStats/Model/Data.swift
@@ -0,0 +1,24 @@
+//
+//  Data.swift
+//  BBallStats
+//
+//  Created by Chad Wallace on 10/27/22.
+//
+
+import Foundation
+
+struct Data {
+    private(set) var totalPoints = 0
+    private(set) var twoPointersMade = 0
+    private(set) var twoPointersAttempted = 0
+    private(set) var threePointersMade = 0
+    private(set) var threePointersAttempted = 0
+    private(set) var rebounds = 0
+    private(set) var assists = 0
+    private(set) var turnovers = 0
+    private(set) var freeThrowsMade = 0
+    private(set) var freeThrowsAttempted = 0
+    
+    
+
+} // end struct Data
---
+++ BBallStats/Views/UpButton.swift
@@ -0,0 +1,30 @@
+//
+//  UpButton.swift
+//  BBallStats
+//
+//  Created by Chad Wallace on 10/27/22.
+//
+
+
+import SwiftUI
+
+struct UpButton: View {
+    var number : Int
+    
+    var body: some View {
+        Text("\(number)")
+            .frame(width: 110, height: 110)
+            .font(.system(size: 40, weight: .bold))
+            .foregroundColor(Color.white)
+            .background(Color.green)
+            .clipShape(Circle())
+            .padding()
+    }
+}
+
+struct UpButton_Previews: PreviewProvider {
+    static var previews: some View {
+        UpButton(number: 100)
+    }
+}
+
